d006f11400958176a1307ae06b6ec1072ea43870
Michael <Michaelflowersdev@gmail.com>
2021-07-06 21:46:58 -0500

    updated--missing indexOf solution

--- app.js
+++ app.js
@@ -7,130 +7,178 @@
 // -Each question should have four multiple-choice answers to choose from.
 // -When the user submits the quiz, add a message underneath each question letting them know if they got it right/wrong (give them the correct answer).
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton)
+let question = {
+    title: '"Who is the best javascript mentor?',
+    alternatives: ['TJ', 'Al Einstein', 'Nik Tesla', 'John Connor'],
+    correctAnswer: 'TJ'
+};
 
+//3. wrap in a function
+function showQuestion(q) {         
+// 1. select dom element
+let titleDiv = document.getElementById('title')
 
-function showQuestions(questions, quizContainer){
-	// we'll need a place to store the output and the answer choices
-	var output = [];
-	var answers;
+//2. modify it 
+titleDiv.textContent = q.title;
 
-	// for each question...
-	for(var i=0; i<questions.length; i++){
-		
-		// first reset the list of answers
-		answers = [];
-
-		// for each available answer to this question...
-		for(letter in questions[i].answers){
-
-			// ...add an html radio button
-			answers.push(
-				'<label>'
-					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-					+ letter + ': '
-					+ questions[i].answers[letter]
-				+ '</label>'
-			);
-		}
-
-		// add this question and its answers to the output
-		output.push(
-			'<div class="question">' + questions[i].question + '</div>'
-			+ '<div class="answers">' + answers.join('') + '</div>'
-		);
-	}
-
-	// finally combine our output list into one string of html and put it on the page
-	quizContainer.innerHTML = output.join('');
+// grab all class alternative**make sure to include the .
+let alts = document.querySelectorAll('.alternative')
+// console.log(alts)
+alts.forEach(function(element, index){
+    //modify to the alternative
+    element.textContent = q.alternatives[index];
+    //6. add click listener for options
+    element.addEventListener('click', function(){
+        // test to make sure click is working -- element.textContent = 'a';
+        // check correct answer -- create a if statement
+        if (q.correctAnswer == index) {
+            console.log('Correct!');
+        } else {
+            alert(q.correctAnswer);
+            console.log('Wrong!');
+        }
+        /////////////////////////////////
+        // need to convert indexOf to string for correct answer.
+        //////////////////////////////
+    });
+});
 }
 
-showQuestions(questions, quizContainer);
+showQuestion(question)
+// 4.
+let btn = document.getElementById('btn')
+//5.
+// add a listening event for click and create a function for ea time
+btn.addEventListener('click', function(){
+    console.log('Clicked!')
+});
+////////////////////////////
+// var score = 0;
+// var questions = [ 
+//     {
+//     prompt: "Who is the best javascript mentor?\n(a) TJ\n\Al Einstein\n\Nik Tesla\n\John Connor",
+//     answer: "a"
+ 
+// },
+//     {
+//     prompt: "What is the most popular javascript library or framework?\n(a) Angular\n\Vue\n\React\n\Other",
+//     answer: "c"
+// },
+//     {
+//     question: "What is the most popular front end programming language?\n(a) Html\n\JavaScript\n\CSS\n\Other",
+//     answer: "b"
+// },
+//  ];
+
+// // for loop to iterate through questions to ask
+//  for (var i = 0; i < questions.length; i ++) {
+//      // initiate prompt ask question to user and get response.
+//      var response = window.prompt(questions[i].prompt);
+//      if(response == questions[i].answer) {
+//          alert("Your right!");
+//      } else {
+//          alert("Your wrong!")
+//      }
+//  }
+
+
+ //////////////////////////////////
 
 
 
-// For each question, find the selected answer
-// If the answer is correct, respond accordingly
-// If the answer is wrong, respond accordingly
-// Show the number of correct answers out of the total
+// var quizContainer = document.getElementById('quiz');
+// var resultsContainer = document.getElementById('results');
+// var submitButton = document.getElementById('submit');
 
-function showResults(questions, quizContainer, resultsContainer){
-	
-	// gather answer containers from our quiz
-	var answerContainers = quizContainer.querySelectorAll('.answers');
-	
-	// keep track of user's answers
-	var userAnswer = '';
-	var numCorrect = 0;
-	
-	// for each question...
-	for(var i=0; i<questions.length; i++){
+// generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton)
 
-		// find selected answer
-		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+
+// function showQuestions(questions, quizContainer){
+// 	// we'll need a place to store the output and the answer choices
+// 	var output = [];
+// 	var answers;
+
+// 	// for each question...
+// 	for(var i=0; i<questions.length; i++){
 		
-		// if answer is correct
-		if(userAnswer===questions[i].correctAnswer){
-			// add to the number of correct answers
-			numCorrect++;
+// 		// first reset the list of answers
+// 		answers = [];
+
+// 		// for each available answer to this question...
+// 		for(letter in questions[i].answers){
+
+// 			// ...add an html radio button
+// 			answers.push(
+// 				'<label>'
+// 					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+// 					+ letter + ': '
+// 					+ questions[i].answers[letter]
+// 				+ '</label>'
+// 			);
+// 		}
+
+// 		// add this question and its answers to the output
+// 		output.push(
+// 			'<div class="question">' + questions[i].question + '</div>'
+// 			+ '<div class="answers">' + answers.join('') + '</div>'
+// 		);
+// 	}
+
+// 	// finally combine our output list into one string of html and put it on the page
+// 	quizContainer.innerHTML = output.join('');
+// }
+
+// showQuestions(questions, quizContainer);
+
+
+
+// // For each question, find the selected answer
+// // If the answer is correct, respond accordingly
+// // If the answer is wrong, respond accordingly
+// // Show the number of correct answers out of the total
+
+// function showResults(questions, quizContainer, resultsContainer){
+	
+// 	// gather answer containers from our quiz
+// 	var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+// 	// keep track of user's answers
+// 	var userAnswer = '';
+// 	var numCorrect = 0;
+	
+// 	// for each question...
+// 	for(var i=0; i<questions.length; i++){
+
+// 		// find selected answer
+// 		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+		
+// 		// if answer is correct
+// 		if(userAnswer===questions[i].correctAnswer){
+// 			// add to the number of correct answers
+// 			numCorrect++;
 			
-			// color the answers green
-			answerContainers[i].style.color = 'lightgreen';
-		}
-		// if answer is wrong or blank
-		else{
-			// color the answers red
-			answerContainers[i].style.color = 'red';
-		}
-	}
+// 			// color the answers green
+// 			answerContainers[i].style.color = 'lightgreen';
+// 		}
+// 		// if answer is wrong or blank
+// 		else{
+// 			// color the answers red
+// 			answerContainers[i].style.color = 'red';
+// 		}
+// 	}
 
-	// show number of correct answers out of total
-	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-}
+// 	// show number of correct answers out of total
+// 	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+// }
 
-// on submit, show results
-submitButton.onclick = function(){
-	showResults(questions, quizContainer, resultsContainer);
-}
+// // on submit, show results
+// submitButton.onclick = function(){
+// 	showResults(questions, quizContainer, resultsContainer);
+// }
 
 
-/////////////////////////////
-const myQuestions = [ 
-    {
-    question: "Who is the best javascript mentor?",
-    answers: {
-        a: "TJ",
-        b: "Al Einstein",
-        c: "Nik Tesla",
-        d: "John Connor"
-    },
-    correctAnswer: "a"
-},
-    {
-    question: "What is the most popular javascript library or framework?",
-    answers: {
-        a: "Angular",
-        b: "Vue",
-        c: "React",
-        d: "Other"
-    },
-    correctAnswer: "c"
-},
-    {
-    question: "What is the most popular front end programming language?",
-    answers: {
-        a: "Html",
-        b: "Javascript",
-        c: "CSS",
-        d: "Other"
-    },
-    correctAnswer: "b"
-},
- ];
+
 
 
 
